c06035f632eedf62c391c834223984955835562e
Jordy <Jordyvazgol@gmail.com>
2019-08-28 09:02:13 -0600

    Initial Commit

---
+++ FoodTracke PT2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracke PT2
+//
+//  Created by Jordy Vazquez on 8/28/19.
+//  Copyright © 2019 Jordy Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
