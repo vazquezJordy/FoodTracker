cbc26c5f836bd0e3feb5b3a61a4c367f990b9b12
Jordy <Jordyvazgol@gmail.com>
2019-08-30 10:02:44 -0600

    got to the connect the ui mid way

--- FoodTracke PT2/ViewController.swift
+++ FoodTracke PT2/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Mark: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "default text"
+    }
 }
 
