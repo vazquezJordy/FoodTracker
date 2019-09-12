b7ffcd174d200ff00a399bce2043c4bc056db4b8
Jordy <Jordyvazgol@gmail.com>
2019-09-12 07:32:20 -0600

    changed UIbutton

--- FoodTracke PT2/ViewController.swift
+++ FoodTracke PT2/ViewController.swift
@@ -8,19 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
-    @IBOutlet weak var nameTextField: UITextField!
+    
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
-
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
     // Mark: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "default text"
+        mealNameLabel.text = "Default Text"
     }
 }
-
